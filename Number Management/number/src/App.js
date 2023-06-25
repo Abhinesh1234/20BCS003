6cd06d0dd74f108315f614f9b9aa831c7ba072dd
Abhinesh1234 <81626221+Abhinesh1234@users.noreply.github.com>
2023-06-25 11:32:04 +0530

    source code

---
+++ Number Management/number/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+
+function App() {
+  const [numbers, setNumbers] = useState([]);
+
+  const fetchNumbers = async () => {
+    try {
+      const urlParams = new URLSearchParams(window.location.search);
+      const urls = urlParams.getAll('url');
+
+      const requests = urls.map(url => axios.get(url));
+      const responses = await Promise.all(requests);
+
+      const mergedNumbers = responses.reduce((merged, response) => {
+        if (response.data && Array.isArray(response.data.numbers)) {
+          merged = Array.from(new Set([...merged, ...response.data.numbers]));
+        }
+        return merged;
+      }, []);
+      const sortedNumbers = mergedNumbers.sort((a, b) => a - b);
+      setNumbers(sortedNumbers);
+
+      setNumbers(mergedNumbers);
+    } catch (error) {
+      console.error('Error fetching numbers:', error);
+    }
+  };
+
+  return (
+    
+    <div>
+      
+   
+
+      <title>Ab</title>
+      <h1>20BCS003 - Number Management</h1>
+      <button onClick={fetchNumbers}>Fetch Numbers</button>
+    <p>numbers= {numbers+ " "}</p>
+      
+
+        
+      
+    </div>
+  );
+}
+
+export default App;
